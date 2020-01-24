903bc00ac0023682c9907d7ad5669320b49fb4bc
John Winstead <jwinstead@launchdarkly.com>
2020-01-24 16:48:23 +0000

    make self updating

--- start.sh
+++ start.sh
@@ -2,21 +2,20 @@
 USER=brooswit
 FREQUENCY=60
 
-while true
+pushd .
+mkdir repositories
+cd repositories
+curl -s https://api.github.com/users/$USER/repos?per_page=200 | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
+
+for D in `find . -type d`
 do
     pushd .
-    mkdir repositories
-    cd repositories
-    curl -s https://api.github.com/users/$USER/repos?per_page=200 | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
-
-    for D in `find . -type d`
-    do
-        pushd .
-        cd $d
-        bash clown.sh
-        popd
-    done
+    cd $d
+    bash clown.sh
     popd
-
-    sleep $FREQUENCY
 done
+popd
+
+sleep $FREQUENCY
+git pull
+bash start.sh
